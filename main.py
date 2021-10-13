b7dc14aee58ea4ebd462daaf4841be8af15be4be
Dariusz Dudek <darek@ebud.pl>
2021-10-13 12:38:44 +0200

    before add db operations

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from views import MainMenu
 
+
 class Application:
     @staticmethod
     def main():
         menu = MainMenu()
         menu.draw()
+
+        option = menu.get_screen()
+        option.draw()
 
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,24 +1,45 @@
-class AddCost:
+from abc import ABC, abstractmethod
+
+
+class AbstractView(ABC):
+    @abstractmethod
+    def draw(self):
+        pass
+
+
+class AddCost(AbstractView):
     SHORTCUT = 'dk'
     LABEL = 'Dodaj koszt'
 
+    def draw(self):
+        print(AddCost.LABEL)
 
-class ListCost:
+
+class ListCost(AbstractView):
     SHORTCUT = 'wk'
     LABEL = 'Wyświetl koszty'
 
+    def draw(self):
+        print(ListCost.LABEL)
 
-class AddIncomes:
+
+class AddIncomes(AbstractView):
     SHORTCUT = 'dp'
     LABEL = 'Dodaj przychód'
 
+    def draw(self):
+        print(AddIncomes.LABEL)
 
-class ListIncomes:
+
+class ListIncomes(AbstractView):
     SHORTCUT = 'wp'
     LABEL = 'Wyświetl przychody'
 
+    def draw(self):
+        print(ListIncomes.LABEL)
 
-class MainMenu:
+
+class MainMenu(AbstractView):
     OPTIONS = {
         AddCost.SHORTCUT: AddCost(),
         ListCost.SHORTCUT: ListCost(),
@@ -26,14 +47,14 @@ class MainMenu:
         ListIncomes.SHORTCUT: ListIncomes()
     }
 
+    def get_screen(self):
+        option = None
+        while option not in MainMenu.OPTIONS:
+            option = input('Wybierz opcję: ')
+
+        return MainMenu.OPTIONS[option]
+
     def draw(self):
         print('Powiedz co chcesz zrobić: ')
         for shortcut, screen in MainMenu.OPTIONS.items():
             print(f'[{shortcut}] - {screen.LABEL}')
-
-        option = None
-
-        while option not in MainMenu.OPTIONS:
-            option = input('Wybierz opcję: ')
-
-        print(MainMenu.OPTIONS[option])
